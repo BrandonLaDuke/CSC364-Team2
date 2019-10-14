57861d518aeef0b2bef6775aac9b5c365860e50f
Brandon LaDuke <brandon@brandonladuke.com>
2019-10-13 23:03:03 -0400

    Added Navigation

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,30 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
 
-var browserSync = require("browser-sync").create();
-
-
-function reload() {
-  browserSync.reload();
-  console.log("did it work?");
+//Complile scss into class
+function style() {
+  // 1. Where is my scss
+  return gulp.src('./scss/**/*.scss')
+  //2 . pass that file through sass compiler
+  .pipe(sass().on('error', sass.logError))
+  //3. Where do I save the complied css?
+  .pipe(gulp.dest('./css'))
+  // 4. Stream changes on all browserSync
+  .pipe(browserSync.stream());
 }
 
 function watch() {
   browserSync.init({
     server: {
-      baseDir: "app",
-      proxy: 'testing.dev'
+      baseDir: './'
     }
-  })
-  gulp.watch('app/**/*.html', reload);
-  gulp.watch('app/**/*.css', reload);
-  gulp.watch('app/js/**/*.js', reload);
-};
+  });
+  gulp.watch('./scss/**/*.scss', style);
+  gulp.watch('./**/*.html').on('change', browserSync.reload);
+  gulp.watch('./**/*.css').on('change', browserSync.reload);
+  gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+}
 
+exports.style = style;
 exports.watch = watch;
